e9f0d6834e48585ffda7ada7fadbd6717ae1c90e
foliobrainskilldev <acaciofariav@gmail.com>
2026-02-17 19:14:00 +0200

    Update MainActivity.java

--- app/src/main/java/org/example/myapplication/MainActivity.java
+++ app/src/main/java/org/example/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package org.example.myapplication;
 
-import android.app.Activity;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isFlashOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnFlash = findViewById(R.id.btnFlash);
+
+        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        btnFlash.setOnClickListener(v -> {
+            try {
+                String cameraId = cameraManager.getCameraIdList()[0];
+                isFlashOn = !isFlashOn;
+                cameraManager.setTorchMode(cameraId, isFlashOn);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
